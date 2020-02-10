2ae13f70665d25e46ceb1375fb94581792685a82
aib <aibok42@gmail.com>
2020-02-10 12:10:09 +0300

    Change the noise function to return 3 separate channels

--- npnoise.py
+++ npnoise.py
@@ -3,14 +3,15 @@ import numpy as np
 class NPNoise:
 	def __init__(self):
 		self.cube_bias = 100
-		self.noise_cube = np.random.random((200, 200, 200))
+		self.noise_cube = np.random.random((200, 200, 200, 3))
 
-	def noise3(self, coord):
+	def noise3x3(self, coord):
 		lerp = lambda a, b, x: a + (b-a)*x
 
 		mins = np.floor(coord).astype(np.int8)
 		maxs = np.ceil(coord).astype(np.int8)
 		ts = coord - mins
+		ts = np.repeat(ts, 3).reshape(-1, 3, 3)
 
 		mins += self.cube_bias
 		maxs += self.cube_bias
@@ -24,14 +25,14 @@ class NPNoise:
 		xYZ = self.noise_cube[mins[:,0],maxs[:,1],maxs[:,2]]
 		XYZ = self.noise_cube[maxs[:,0],maxs[:,1],maxs[:,2]]
 
-		lyz = lerp(xyz, Xyz, ts[:,0])
-		lYz = lerp(xYz, XYz, ts[:,0])
-		lyZ = lerp(xyZ, XyZ, ts[:,0])
-		lYZ = lerp(xYZ, XYZ, ts[:,0])
+		lyz = lerp(xyz, Xyz, ts[:,0,:])
+		lYz = lerp(xYz, XYz, ts[:,0,:])
+		lyZ = lerp(xyZ, XyZ, ts[:,0,:])
+		lYZ = lerp(xYZ, XYZ, ts[:,0,:])
 
-		llz = lerp(lyz, lYz, ts[:,1])
-		llZ = lerp(lyZ, lYZ, ts[:,1])
+		llz = lerp(lyz, lYz, ts[:,1,:])
+		llZ = lerp(lyZ, lYZ, ts[:,1,:])
 
-		lll = lerp(llz, llZ, ts[:,2])
+		lll = lerp(llz, llZ, ts[:,2,:])
 
 		return lll
